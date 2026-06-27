adcbab57fb867c050e9a31c4eb957597c1404913
K-Owen983 <k.owendatascience@gmail.com>
2026-06-26 21:24:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 
+
+const cookieBanner=document.getElementById("cookie-banner");
+
+const accept=document.getElementById("accept-cookies");
+
+const reject=document.getElementById("reject-cookies");
+
+const settings=document.getElementById("settings-cookies");
+
+if(!localStorage.getItem("cookies-choice")){
+
+cookieBanner.style.display="block";
+
+}
+
+accept.onclick=function(){
+
+localStorage.setItem("cookies-choice","accepted");
+
+cookieBanner.style.display="none";
+
+}
+
+reject.onclick=function(){
+
+localStorage.setItem("cookies-choice","rejected");
+
+cookieBanner.style.display="none";
+
+}
+
